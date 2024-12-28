bb3c491c0aec92b993df3273738f2641ed39e5a7
jubayerCodes <jubayerhossain111220@gmail.com>
2024-12-28 15:07:21 +0600

    updated skills logo

--- src/pages/Home/Skills/SingleSkill/SingleSkill.jsx
+++ src/pages/Home/Skills/SingleSkill/SingleSkill.jsx
@@ -1,13 +1,14 @@
 
-import logo from '../../../../assets/images/skills/react.png'
 import './SingleSkill.css'
 
-const SingleSkill = () => {
+const SingleSkill = ({ skill }) => {
+
+    const { name, logo } = skill
 
     return (
-        <div className=' bg-[#000] py-2 px-5 flex justify-start items-center gap-3 rounded-lg'>
-            <img src={logo} className='w-[40px]' alt="" />
-            <h3 className='text-lg text-white capitalize'>React js</h3>
+        <div className=' bg-[#000] py-2 px-5 flex justify-start items-center gap-3 rounded-lg h-16'>
+            <img src={logo} className='max-w-[30px] max-h-[40px]' alt="" />
+            <h3 className='text-lg text-white capitalize'>{name}</h3>
         </div>
     );
 };
--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -1,25 +1,73 @@
 
-import React from 'react';
+
 import SingleSkill from './SingleSkill/SingleSkill';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import reactLogo from '../../../assets/images/skills/react.png'
+import nextLogo from '../../../assets/images/skills/nextjs.png'
+import jsLogo from '../../../assets/images/skills/js.png'
+import nodeLogo from '../../../assets/images/skills/node-js.png'
+import mongodbLogo from '../../../assets/images/skills/mongodb.png'
+import expressLogo from '../../../assets/images/skills/express-js.png'
+import firebaseLogo from '../../../assets/images/skills/firebase.png'
+import tailwindLogo from '../../../assets/images/skills/tailwind-css.png'
+import materialLogo from '../../../assets/images/skills/material-ui.png'
+import bootstrapLogo from '../../../assets/images/skills/bootstrap.png'
 
 const Skills = () => {
+
+    const skills = [
+        {
+            name: 'React JS',
+            logo: reactLogo
+        },
+        {
+            name: 'Next JS',
+            logo: nextLogo
+        },
+        {
+            name: 'Javascript',
+            logo: jsLogo
+        },
+        {
+            name: 'Node JS',
+            logo: nodeLogo
+        },
+        {
+            name: 'Mongo DB',
+            logo: mongodbLogo
+        },
+        {
+            name: 'Express JS',
+            logo: expressLogo
+        },
+        {
+            name: 'Firebase',
+            logo: firebaseLogo
+        },
+        {
+            name: 'Tailwind CSS',
+            logo: tailwindLogo
+        },
+        {
+            name: 'Material UI',
+            logo: materialLogo
+        },
+        {
+            name: 'Bootstrap',
+            logo: bootstrapLogo
+        },
+    ]
+
+
     return (
         <section className='section'>
             <div className="my-container">
                 <SectionTitle bgTitle={'skills'} title={'skills'} />
 
                 <div className='grid grid-cols-5 justify-between md:items-center mt-12 xl:mt-32 gap-5 xl:gap-10'>
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
-                        <SingleSkill />
+                    {
+                        skills?.map((skill, idx) => <SingleSkill skill={skill} key={idx} />)
+                    }
                 </div>
             </div>
         </section>
